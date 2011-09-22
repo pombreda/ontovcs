02bfc9e0683b3f8f57de4b9654781d224c097435
utapyngo <utapyngo@gmail.com>
2011-09-22 16:27:10 +0700

    Moved args4j to separate directory. setup.cmd now uses setx and sets PATH for current session If swt.jar is absent, owl2merge will complain and ask user to download it.

--- bin/owl2merge.cmd
+++ bin/owl2merge.cmd
@@ -1,2 +1,12 @@
 @echo off
-java -jar %~dp0\owl2merge.jar %*
+setlocal enabledelayedexpansion
+pushd %~dp0\..\lib\swt
+for /f %%d IN ('cd') do set DIR=%%d
+popd
+if exist !DIR!\swt.jar (
+    java -jar %~dp0\owl2merge.jar %*
+) else (
+    echo Please, download SWT for your operating system and copy swt.jar to !DIR!
+    start http://www.eclipse.org/swt/
+    pause
+)
