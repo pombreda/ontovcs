46d41baaa1dae0982941329828844cb419afe09f
utapyngo <utapyngo@gmail.com>
2011-09-28 23:40:03 +0700

    Fixed a bug: %mem% -> !mem! in owl2merge.cmd

--- bin/owl2merge.cmd
+++ bin/owl2merge.cmd
@@ -5,7 +5,7 @@ for /f %%d IN ('cd') do set DIR=%%d
 popd
 if exist !DIR!\swt.jar (
     for /f %%a in ('%~dp0\javamem.cmd') do set mem=%%a
-    java -Xmx%mem%m -jar %~dp0\owl2merge.jar %*
+    java -Xmx!mem!m -jar %~dp0\owl2merge.jar %*
 ) else (
     echo Please, download SWT for your operating system and copy swt.jar to !DIR!
     start http://www.eclipse.org/swt/
