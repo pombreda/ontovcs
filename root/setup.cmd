2a6df56b0877ad65443e7e687d6536fa94170a20
utapyngo <utapyngo@gmail.com>
2011-09-28 10:19:52 +0700

    Samples: ontovcs ontology from w3.org; setup.cmd uses setx only if it finds it; otherwise,  reg add is used.

--- root/setup.cmd
+++ root/setup.cmd
@@ -1,12 +1,8 @@
 @echo off
 rem // This script adds bin directory to user PATH
-rem // If you need a system-wide setup, change %key% to a long key below,
-rem // and run this script as administrator
 
 setlocal enabledelayedexpansion
 set key=HKEY_CURRENT_USER\Environment
-rem // the long key is for system-wide setup
-rem // set key=HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Environment
 set value=PATH
 set DIR=%~dp0bin
 for /f "tokens=2,*" %%a in ('reg query "%key%" /v "%value%" ^| findstr /c:"%value%"') do (
@@ -15,8 +11,12 @@ for /f "tokens=2,*" %%a in ('reg query "%key%" /v "%value%" ^| findstr /c:"%valu
 echo !data! | findstr %DIR% > nul
 if errorlevel 1 (
 	echo Setting %key%\%value% to "!data!;%DIR%"
-	rem reg add "%key%" /v "%value%" /t "REG_EXPAND_SZ" /d "!data!;%DIR%"
-	setx PATH "!data!;%DIR%"
+	setx 2> nul
+	if errorlevel 9009 (
+		reg add "%key%" /v "%value%" /t "REG_EXPAND_SZ" /d "!data!;%DIR%"
+	) else (
+		setx PATH "!data!;%DIR%"
+	)
 ) else (
 	echo %DIR% is already in PATH
 )
