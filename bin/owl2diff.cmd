246a71f6c07873dc8e0aa6d40d6f7b12e869e3f3
utapyngo <utapyngo@gmail.com>
2011-11-04 22:28:33 +0700

    + InnoSetup installer. * Fixes related to spaces in OntoVCS path.

--- bin/owl2diff.cmd
+++ bin/owl2diff.cmd
@@ -1,2 +1,2 @@
-@for /f %%a in ('%~dp0\javamem.cmd') do @set mem=%%a
-@java -Xmx%mem%m -jar %~dp0\owl2diff.jar %*
+@for /f %%a in ('"%~dp0javamem.cmd"') do @set mem=%%a
+@java -Xmx%mem%m -jar "%~dp0owl2diff.jar" %*
