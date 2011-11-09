821d9423f09b9d6fdc4f6150cecfacb51b0e76ef
utapyngo <utapyngo@gmail.com>
2011-11-09 12:06:50 +0700

    * setup.cmd now installs system-wide if run with admin privileges. * owl2enable.cmd now uses findstr instead of find. * owl2enable now tells what filetypes have been enabled.

--- antbuild/upload.cmd
+++ antbuild/upload.cmd
@@ -1,2 +1,2 @@
 call gcupload -s "Binaries and sources" -p ontovcs -l Featured,Type-Archive,OpSys-All ontovcs-@build.version@.tar.gz
-call gcupload -s "Installer (includes SWT)" -p ontovcs -l Featured,Type-Installer,OpSys-Windows OntoVCS-@build.version@.exe
+call gcupload -s "User-mode installer (includes SWT)" -p ontovcs -l Featured,Type-Installer,OpSys-Windows OntoVCS-@build.version@.exe
